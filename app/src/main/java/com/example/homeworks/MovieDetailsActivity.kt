b438eee1673efa52a5dd250f27e1e37ec8a5d16a
Mykola <mykola.tsykalovskiy@ikirupeople.com>
2020-11-30 00:34:28 +0000

    second homework

---
+++ app/src/main/java/com/example/homeworks/MovieDetailsActivity.kt
@@ -0,0 +1,6 @@
+package com.example.homeworks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MovieDetailsActivity : AppCompatActivity(R.layout.activity_movie_details)

655a40727293ae913b47de360e97826b959c9cf6
Mykola <mykola.tsykalovskiy@ikirupeople.com>
2020-12-07 00:14:03 +0000

    initial commit homework 3

---
+++ app/src/main/java/com/example/homeworks/FragmentMoviesList.kt
@@ -0,0 +1,50 @@
+package com.example.homeworks
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+
+class FragmentMoviesList : Fragment() {
+
+    private var movieDetailsLink: ImageView? = null
+
+    private var listener: MovieDetailsListener? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_movies_list, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        movieDetailsLink = view.findViewById<ImageView>(R.id.main_poster).apply {
+            setOnClickListener { listener?.openMovie() }
+        }
+    }
+
+    fun setMovieDetailsListener(movieDetailsListener: MovieDetailsListener?){
+        listener = movieDetailsListener
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is MovieDetailsListener){
+            listener = context
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    interface MovieDetailsListener{
+        fun openMovie()
+    }
+}
--- app/src/main/java/com/example/homeworks/MovieDetailsActivity.kt
+++ app/src/main/java/com/example/homeworks/MovieDetailsActivity.kt
@@ -3,4 +3,4 @@ package com.example.homeworks
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MovieDetailsActivity : AppCompatActivity(R.layout.activity_movie_details)
+class MovieDetailsActivity : AppCompatActivity(R.layout.fragment_movies_list)
